a191439c661d6c9ffbf22fbee57edd3048604044
Your Name <your.email@example.com>
2026-04-17 16:22:12 +0900

    auto-commit

--- techstory/js/ecma6_object.js
+++ techstory/js/ecma6_object.js
@@ -1,70 +1,83 @@
-//console.log("Hello World!"); 
-// 재활용 가능한 타입
+// console.log("test");
 // 1. 프로토타입을 이용하여 객체를 작성하는 방법
-let std1 ={
-  name: "이순신",
-  score: 100,
-};
-let std2 ={
-  name: "정약용",
-  score: 90,
-};
-let std1 = new Student("이순신", 100);
-let std2 = new Student("정약용", 90);
+// function Student(name, score) {
+//     this.name = name;
+//     this.score = score;
+// }
+
+// let std1 = new Student("홍길동", 100);
+// let std2 = new Student("손흥민", 90);
+
+// console.log(std1, std2);
+
+// 2. 생성자를 포함한 ECMA6 문법
+class Student{
+    constructor(name, score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+let std1 = new Student("홍길동", 100);
+let std2 = new Student("손흥민", 90);
+
 console.log(std1, std2);
 
-//2. 생성자를 포함한 ecma6의 객체 작성 방법
-
-class Student {
-  constructor(name, score) {
-    this.name = name;
-    this.score = score;
-  }
-}
-let std3 = new Student("홍길동", 100);
-let std4 = new Student("김철수", 90);
-console.log(std1, std2, std3, std4);
-
-//3. 객체의 속성을 반복문으로 전부 읽어오기, for in
-let student = [std1, std2, std3, std4];
-for (const element of student) {
-  console.log(element);
-} 
-for (const key in student) {
-  const element = student[key];
-  console.log(element);
+// 3. 객체를 배열에 저장한 후 배열의 원소인 객체의 모든 속성을 반복문으로 
+//    전부 읽어오기 : for of / for in
+let students = [std1, std2];
+for (const student of students) {
+    for (const key in student) {       
+        console.log(student[key]);
+    }
 }
 
-//4. propertu, method 를 포함한 객체 작성 방법
+// 4. property, method 를 포함한 객체 생성해보기
+let account =  {
+    owner : "홍길동",
+    account_no : "1111",
+    balance : 10000,
+    withdraw : ()=>{
+        console.log("출금처리");
+    },
+    deposit : () => {
+        console.log("입금처리");
+    },
+    inquiry : () => {
+        console.log("조회처리");
+    }
+};
 
 class Account {
-  constructor(owner, account_no, balance) {
-    this.owner = owner;
-    this.account_no = account_no;
-    this.balance = balance;
-  }
+    constructor(owner, account_no, balance){
+        this.owner = owner;
+        this.account_no = account_no;
+        this.balance = balance;
+    }
 
-  deposit(money) {
-    console.log("입금이 완료되었습니다.");
-    this.balance += money;
-  }
-  inquiry(){
-    console.log("조회처리 되었습니다.", this.balance); 
-  }
-  withdraw(money){
-    console.log("출금이 완료되었습니다.");
-   if(this.balance >= money){
-    this.balance -= money;
-   }else{
-    console.log("출금할 수 없습니다.");
-   }
-  }   
+    deposit(money) {
+        console.log("입금처리");
+        this.balance += money;
+    }
+
+    inquiry() {
+        console.log("조회처리 : " , this.balance);
+    }
+
+    withdraw(money) {
+        console.log("출금처리");
+        if(this.balance >= money) {
+            this.balance -= money;
+        } else {
+            console.log("출금할 수 없습니다.");
+        }
+    }
 }
 
-let acc1 = new Account("홍길동", "1111", 100000);
-let acc2 = new Account("김철수", "2222", 200000);
+let acc1 = new Account("홍길동", "1111", 10000);
+let acc2 = new Account("손흥민", "2222", 20000);
 
-acc1.deposit(100000);
+acc1.deposit(10000);
 acc1.inquiry();
-acc1.withdraw(200000);
+acc1.withdraw(20000);
 acc1.inquiry();
