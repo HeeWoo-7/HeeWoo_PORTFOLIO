a1fc4be37f6e53e4ca4c3eb97ec6548d22dfdf50
Your Name <your.email@example.com>
2026-04-17 16:28:55 +0900

    auto-commit

---
+++ techstory/js/json.js
@@ -0,0 +1,17 @@
+const jsObj = {
+    name : "David",
+    age : 20
+}
+const json = {
+     "name" : "David",
+    "age" : 20
+}
+console.log(typeof(jsObj))
+
+let str = JSON.stringify(jsObj);
+console.log(typeof(jsOb))
+
+let obj = JSON.parse(str);
+console.log(typeof(obj))
+obj.age;
+obj.name;
